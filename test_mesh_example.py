cedf896a90559c02c556e59fc030a0b1911aa4c7
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2012-04-24 09:38:22 -0400

    Removed a bug in test_mesh_example

--- test_mesh_example.py
+++ test_mesh_example.py
@@ -13,19 +13,19 @@ from mesh_class_example import MeshExample
 mesh_example = MeshExample(1,3) # topo_dim = 1, geom_dim = 3
 
 num_vertices = 750
-num_segments = 1
+num_segments = num_vertices
 
 mesh_example.initializing_empty_grid(num_vertices, num_segments)
 mesh_example.create_vertices(10,15,5)
 
 # make the cells
-mesh_example.create_cells(num_vertices)
+mesh_example.create_cells(num_segments)
 
 fx=File('test_data/outmesh_topo1.xml')
 fx << mesh_example.mesh
 
-fr = File('test_data/outmesh.raw')
-fr << mesh_example.mesh
+#fr = File('test_data/outmesh.raw')
+#fr << mesh_example.mesh
 
 
 # TODO: Repeat for other meshes (i.e. (lon_u, lat_u, s_rho), etc)
