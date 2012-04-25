d9c2aa8520d13a66133c0ccde083161d42f23073
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2012-04-25 14:43:37 -0400

    Added create_mesh_topo method to the class, MeshCoordinateAxes, so that it can creates its own mesh. This is a step towards making the MeshCoordinateAxes class more complete. Also

--- mesh_class_example.py
+++ mesh_class_example.py
@@ -1,5 +1,6 @@
 
 from dolfin import *
+from test_mesh_roms import get_coord_array, create_mesh
 
 class MeshExample(object):
     """
@@ -110,16 +111,76 @@ class Variable(object):
     Connect a variable to a time vertex
     '''
 
-    def __init__(self, variable_name, time_vertex, time_mesh):
+    def __init__(self, variable_name, time_vertex_index, mesh_topo):
         self.name = variable_name
-        self.time_vertex = time_vertex
-        self.time_mesh = time_mesh
+        self.time_vertex_index = time_vertex_index
+        self.time_mesh = mesh_topo.time_mesh
+        self.mesh_topo = mesh_topo
+
+    def create_variable(self):
+        self.filename = self.make_meshfunction_filename(self.name, self.time_vertex_index)
+        self.variable_handle = MeshFunction("double", self.mesh_topo, 0)
+        return self.variable_handle
+
+    def store_values(self, ds):
+        '''
+        Store values for that variable from a dataset
+        '''
+        self.variable_handle.array()[:] = ds.variables[self.name][0,:,:,:].flatten()
+
+    def make_meshfunction_filename(self):
+        '''
+        Construct a name for the topological coordinate axes
+        '''
+        name = self.name + '_' + str(self.time_vertex_index)
+        return name
+
+    def write_to_disk(self):
+        temp_outfile = File('test_data/' + self.filename + '.xml')
+        temp_outfile << self.variable_handle
 
 def MeshCoordinateAxes(object):
     '''
     Connects a coordinate system with a time domain
     '''
-    def __init__(self, name, time_domain):
+    def __init__(self, name, time_domain, time_mesh):
         self.name = name
         self.time_domain = time_domain
+        self.time_mesh = time_mesh
+
+        if self.name == 'mesh_topo_1':
+            self.x_coord = 'lon_rho'
+            self.y_coord = 'lat_rho'
+            self.z_coord = 's_rho'
+        elif self.name == 'mesh_topo_2':
+            self.x_coord = 'lon_u'
+            self.y_coord = 'lat_u'
+            self.z_coord = 's_rho'
+        elif self.name == 'mesh_topo_3':
+            self.x_coord = 'lon_v'
+            self.y_coord = 'lat_v'
+            self.z_coord = 's_rho'
+        elif self.name == 'mesh_topo_4':
+            self.x_coord = 'lon_rho'
+            self.y_coord = 'lat_rho'
+            self.z_coord = 's_w'
+
+    def create_mesh_topo(self):
+
+        # invoking the function below creates a mesh and writes it to a disk also
+        mesh_topo = create_mesh('test_data/' + make_topo_filename() + '.xml',
+                                    topo_dim = 1, geom_dim = 3, 
+                                    x_coord = self.x_coord, 
+                                    y_coord = self.y_coord, 
+                                    z_coord = self.z_coord)
+
+        return mesh_topo
+
+    def make_topo_filename(self):
+        '''
+        Construct a name for the topological coordinate axes
+        '''
+        name = self.name + '_' + str(self.time_domain.unique_value)
+        return name
+
 
